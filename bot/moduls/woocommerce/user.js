6471f04205e844f2e482fead65e38b917aec6616
Navid shad <navidshad72@gmail.com>
2018-06-10 18:01:24 +0430

    debug

--- bot/moduls/woocommerce/user.js
+++ bot/moduls/woocommerce/user.js
@@ -211,7 +211,7 @@ var showDirectory = async function(user, category, page, optionparam)
     var speratedSection = user.section.split('/');
     //speratedSection = speratedSection.slice(0, speratedSection.length-1);
     var parent = speratedSection[speratedSection.length-1];
-    var back = (parent === fn.str['mainMenu']) ? fn.str['backToMenu'] : fn.mstr.category['backtoParent'];
+    var back = fn.str['backToMenu']; //(parent === fn.str['mainMenu']) ? fn.str['backToMenu'] : fn.mstr.category['backtoParent'];
     var remarkup = fn.generateKeyboard({'custom':true, 'grid':true, 'list':list}, false, columns);
 
     // navigator
